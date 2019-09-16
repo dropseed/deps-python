c6e87af546600bc2b577b5fc2e0f2ac5cf87c5e0
Dave Gaeddert <dave.gaeddert@gmail.com>
2019-09-16 09:49:58 -0500

    Fix self.dir ""

--- src/models.py
+++ src/models.py
@@ -13,7 +13,7 @@ class Manifest:
 
     def __init__(self, filename):
         self.filename = filename
-        self.dir = os.path.dirname(self.filename)
+        self.dir = os.path.dirname(self.filename) or os.getcwd()
         if filename.endswith(self.PIPFILE):
             self.type = self.PIPFILE
             self.filewriter = dparse.updater.PipfileUpdater
